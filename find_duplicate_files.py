1fec3c4289080f1067ae1fefd2afafc94466a313
Shekcon <quangsang9773@gmail.com>
2019-02-27 23:20:13 +0700

    - Update group files by size more readable - Add group files by checksum

--- find_duplicate_files.py
+++ find_duplicate_files.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import hashlib
+from hashlib import md5
 import os
 from argparse import ArgumentParser
 
@@ -23,10 +23,24 @@ def scan_files(directory):
 def group_files_by_size(file_path_names):
     files_same_size = {}
     for file in file_path_names:
-        size = os.stat(file).st_size
-        if size:
-            files_same_size.setdefault(size, []).append(file)
-    return [same_size for _, same_size in files_same_size.items() if len(same_size) > 1]
+        files_same_size.setdefault(os.stat(file).st_size, []).append(file)
+    return [same_size for size, same_size in files_same_size.items() if len(same_size) > 1 and size]
+
+
+def get_file_checksum(name_file):
+    '''
+    Task: return hash md5 of file passed
+    '''
+    with open(file, 'rb') as f:
+        return md5(f.read()).hexdigest()
+    
+
+def group_files_by_checksum(file_path_names):
+    files_same_checksum = {}
+    for file in file_path_names:
+        checksum = get_file_checksum(file)
+        files_same_size.setdefault(checksum, []).append(file)
+    return [same_checksum for _, same_checksum in files_same_checksum.items() if len(same_checksum) > 1]
 
 
 def main():
