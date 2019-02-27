46a9e09b087919396431a6d92cab4950a2c6fafd
Shekcon <quangsang9773@gmail.com>
2019-02-27 23:04:43 +0700

    - Add find files have same size more than ones expect size of files is 0 byte

--- find_duplicate_files.py
+++ find_duplicate_files.py
@@ -12,18 +12,27 @@ def handle_args():
     
 
 def scan_files(directory):
-    print(directory)
-    for root, dirs, files in os.walk(directory):
+    base_path = os.path.abspath(directory)
+    all_files = []
+    for root, dirs, files in os.walk(base_path):
         for name in files:
-            print(os.path.join(root, name))
-        for name in dirs:
-            print(os.path.join(root, name))
+            all_files.append(os.path.join(root, name))
+    return all_files
+
+
+def group_files_by_size(file_path_names):
+    files_same_size = {}
+    for file in file_path_names:
+        size = os.stat(file).st_size
+        if size:
+            files_same_size.setdefault(size, []).append(file)
+    return [same_size for _, same_size in files_same_size.items() if len(same_size) > 1]
 
 
 def main():
     args = handle_args()
-    path = args.path
-    scan_files(path)
+    files = scan_files(args.path)
+    group_files_by_size()
 
 if __name__ == "__main__":
     main()
